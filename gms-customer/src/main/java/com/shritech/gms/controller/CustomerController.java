ac380244b96d7d5a3ed803c499f5ceaa8b447478
shripradhan <40176281+shripradhan@users.noreply.github.com>
2022-01-29 22:43:11 +0530

    Added Customer Repository

--- gms-customer/src/main/java/com/shritech/gms/controller/CustomerController.java
+++ gms-customer/src/main/java/com/shritech/gms/controller/CustomerController.java
@@ -3,10 +3,7 @@ package com.shritech.gms.controller;
 import com.shritech.gms.dto.CustomerDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/v1/custoers")
@@ -17,8 +14,16 @@ public class CustomerController {
     }
 
     @GetMapping("/get/{customerId}")
+    public ResponseEntity<?> saveCustomer(@RequestBody CustomerDTO customerDTO) {
+        CustomerDTO customer = new CustomerDTO(101L, "Shrikant Ashok Pradhan");
+        return new ResponseEntity<>("Customer save successfully",HttpStatus.OK);
+    }
+
+    @GetMapping("/get/{customerId}")
     public ResponseEntity<CustomerDTO> getCustomerById(@PathVariable("customerId") Long customerId){
-        CustomerDTO customer = new CustomerDTO(101l, "Shrikant Ashok Pradhan");
+        CustomerDTO customer = new CustomerDTO(101L, "Shrikant Ashok Pradhan");
         return new ResponseEntity<>(customer, HttpStatus.OK);
     }
+
+
 }
